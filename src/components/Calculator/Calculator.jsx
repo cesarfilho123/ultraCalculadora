aa190dc5e066ac7feeda2446fe6649d462c40a5a
César Filho <cesarfilho09.cf@gmail.com>
2020-12-09 14:53:22 -0300

    estrutura inicial

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Calculator.css'
+// import { Container } from './styles';
+
+class Calculator extends Component {
+    render(){
+        return (
+            <>
+            <h1>
+                Calculadora
+            </h1>
+            <div className="calculator">
+
+            </div>
+            </>
+        )
+    }
+}
+
+export default Calculator;
